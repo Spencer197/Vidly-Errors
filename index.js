9c2699bc7fba1336d74b38ad3de15b67694c4532
Spencer197 <spncr@protonmail.com>
2023-11-20 20:42:07 +0900

    Add NPM winston error logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require(express-async-errors);//Downloaded NPM module - Don't need to store result in const - optional approach 
+require(express-async-errors);//Downloaded NPM module - Don't need to store result in const - optional approach
+const winston = require('winston'); 
 const error = require('./middle/error');
 const config = require('config');
 const Joi = require('joi');
@@ -12,6 +13,8 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+winston.add(winston.transport.File, { filename: 'logfile.log'});
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
